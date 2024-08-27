428e29cd3142854f8d1a661163619e15d5d64cfa
santiagopc08 <santiagoperezcastro365@gmail.com>
2024-08-27 10:58:04 -0500

    tank movement implemented, bullet added

--- tank.gd
+++ tank.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_key_pressed(KEY_W):
+		position += Vector2(0, -1)
+	if Input.is_key_pressed(KEY_S):
+		position += Vector2(0, 1)
+	if Input.is_key_pressed(KEY_A):
+		position += Vector2(-1, 0)
+	if Input.is_key_pressed(KEY_D):
+		position += Vector2(1, 0)
